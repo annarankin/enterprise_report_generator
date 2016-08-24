dff48350b554097ef64856cb742561c6e3b72d4b
Anna Rankin <anna@annarankin.com>
2016-08-24 18:10:25 -0400

    Simple app get

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 import styles from './styles/styles.scss'
+import React, { Component } from 'react'
+import ReactDOM, { render } from 'react-dom'
+
+class App extends Component {
+  render() {
+    return(
+      <div className="app">
+        <h1>Welcome! &hearts;</h1>
+      </div>
+      )
+  }
+}
+
+render(<App/>, document.getElementById('app'))
